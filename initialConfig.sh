cfd557d629e5ea69fd474dc9b635f983f4066b36
garjones <gareth@gareth.com>
2022-01-05 08:26:26 -0800

    Update initialConfig.sh

--- initialConfig.sh
+++ initialConfig.sh
@@ -149,20 +149,13 @@ defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -boolean false
 defaults write com.apple.finder ShowRemovableMediaOnDesktop -boolean false
 defaults write com.apple.finder ShowMountedServersOnDesktop -boolean false
 defaults write com.apple.finder NewWindowTarget PfHm
-defaults write com.apple.finder NewWindowTargetPath 'file:///Users/garjones'
+defaults write com.apple.finder NewWindowTargetPath $HOME
 killall Finder
 echo
 
 # set terminal preferences
-#
-# i have previously modified the terminal PRO theme to my personal settings and then exported it using ..
-# plutil -extract Window\ Settings.Pro xml1 -o - ~/Library/Preferences/com.apple.Terminal.plist > terminal-pro.xml
-#
-# now import those settings and set PRO to be the default & startup themes
-terminalpro=$(curl -fsSL https://raw.githubusercontent.com/garjones/macOS-config/main/terminal-pro.xml)
-plutil -replace Window\ Settings.Pro -xml "$terminalpro" ~/Library/Preferences/com.apple.Terminal.plist
-defaults write com.apple.Terminal "Default Window Settings" -string "Pro"
-defaults write com.apple.Terminal "Startup Window Settings" -string "Pro"
+rm -rf $HOME/Library/Preferences/com.apple.Terminal.plist
+wget https://raw.githubusercontent.com/garjones/macOS-config/main/com.apple.Terminal.plist  --directory-prefix=$HOME/Library/Preferences
 
 # kill the terminal otherwise it will overwrite changes
 read -p "Installation complete. About to kill the terminal ... "
