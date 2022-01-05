f377e8815269b8805fb5378d675ec31ca064ff75
garjones <gareth@gareth.com>
2022-01-04 23:52:51 -0800

    Update initialConfig.sh

--- initialConfig.sh
+++ initialConfig.sh
@@ -154,12 +154,13 @@ killall Finder
 echo
 
 # set terminal preferences
-# to create a terminal theme in xml format
-# plutil -extract Window\ Settings.ProBig xml1 -o - ~/Library/Preferences/com.apple.Terminal.plist > terminal-pro.xml
-
-terminalpro=$(<terminal-pro.xml)
+#
+# i have previously modified the terminal PRO theme to my personal settings and then exported it using ..
+# plutil -extract Window\ Settings.Pro xml1 -o - ~/Library/Preferences/com.apple.Terminal.plist > terminal-pro.xml
+#
+# now import those settings and set PRO to be the default & startup themes
+terminalpro=$(curl -fsSL https://raw.githubusercontent.com/garjones/macOS-config/main/terminal-pro.xml)
 plutil -replace Window\ Settings.Pro -xml "$terminalpro" ~/Library/Preferences/com.apple.Terminal.plist
-
 defaults write com.apple.Terminal "Default Window Settings" -string "Pro"
 defaults write com.apple.Terminal "Startup Window Settings" -string "Pro"
 
